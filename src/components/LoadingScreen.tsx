45dee5a2f80ceceaf3b89e5a595eaa5a234122fa
ndizeyedavid <davidndizeye101@gmail.com>
2026-05-12 20:19:32 +0200

    chore: redesigned a minimal loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { View, Text, ActivityIndicator } from "react-native";
-import { StatusBar } from "expo-status-bar";
 import { APP_CONFIG } from "../constants";
-
+// @ts-ignore
+import Ionicons from "@expo/vector-icons/Ionicons";
 export default function LoadingScreen({
   onLoadingComplete,
 }: {
@@ -17,15 +17,13 @@ export default function LoadingScreen({
   }, [onLoadingComplete]);
 
   return (
-    <View className="flex-1 bg-blue-600 items-center justify-center">
+    <View className="flex-1 bg-white items-center justify-center">
       <View className="items-center">
-        <ActivityIndicator size="large" color="white" className="mb-4" />
-        <Text className="text-3xl font-bold text-white mb-2">InnSync</Text>
-        <Text className="text-white opacity-80">
-          Loading your experience...
-        </Text>
+        <Ionicons name="planet-outline" size={90} className="mb-8" />
+        <Text className="text-3xl font-bold  mb-2">InnSync</Text>
+        {/* <Text className=" opacity-80">Loading your experience...</Text> */}
+        <ActivityIndicator size="small" className="mt-4" />
       </View>
-      {/* <StatusBar style="light" /> */}
     </View>
   );
 }
